311c228693083e72d63470959e404716593c0cb6
godlike_panos <godlike_panos@cf34f53d-7049-02d2-63c7-51c127bf19f8>
2009-12-15 13:07:56 +0000

    Reverting

--- trunk/is_lp_spot.glsl
+++
@@ -1,5 +0,0 @@
-#define _SPOT_LIGHT_
-
-#pragma anki uniform light_tex 5
-
-#pragma anki include "shaders/is_lp_generic.glsl"
--- trunk/ms_mp_dnsk.glsl
+++
@@ -1,6 +0,0 @@
-#define _HAS_DIFFUSE_MAP_
-#define _HAS_NORMAL_MAP_
-#define _HAS_SPECULAR_MAP_
-#define _HARDWARE_SKINNING_
-
-#pragma anki include "shaders/ms_mp_generic.glsl"
