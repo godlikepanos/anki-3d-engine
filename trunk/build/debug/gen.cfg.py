a6822b8690c99f652e5a76383043631a101e89d5
godlike_panos <godlike_panos@cf34f53d-7049-02d2-63c7-51c127bf19f8>
2010-02-09 23:05:36 +0000

    Adding the build directory (at last)

---
+++ trunk/build/debug/gen.cfg.py
@@ -0,0 +1,9 @@
+include_paths = [ "../../src/math/", "../../src/tokenizer/", "../../src/uncategorized/", "../../src/", "../../src/renderer/", "../../src/scene/", "../../src/resources/", "../../src/utility/" ]
+#precompiled_headers = ["../../src/uncategorized/common.h"]
+precompiled_headers = []
+project_name = "AnKi"
+compiler = "g++"
+common_flags = ""
+compiler_flags = "-c -pedantic-errors -pedantic -ansi -Wall -Wextra -W -pipe -O0 -g3 -pg `sdl-config --cflags` -D_DEBUG_ -D_TERMINAL_COLORING_ -D_PLATFORM_LINUX_ -I../../../glew/include"
+precompiled_headers_flags = "-x c++-header"
+linker_flags = "-lGL -lGLU -lSDL_image `sdl-config --static-libs` -ljpeg -lGLEW -L../../../glew/lib"
