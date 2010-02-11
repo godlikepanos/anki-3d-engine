31bfecb939fc8fd8f76187eb439e7b73757d1696
godlike_panos <godlike_panos@cf34f53d-7049-02d2-63c7-51c127bf19f8>
2010-02-11 08:30:51 +0000

    The genmakefile.py now has support for conflicting object files. You can now have the same name to multiple .cpp files without conflicts in the build process

--- trunk/build/debug/gen.cfg.py
+++ trunk/build/debug/gen.cfg.py
@@ -1,9 +1,9 @@
-include_paths = [ "../../src/math/", "../../src/tokenizer/", "../../src/uncategorized/", "../../src/", "../../src/renderer/", "../../src/scene/", "../../src/resources/", "../../src/utility/" ]
+include_paths = [ "../../src/math/", "../../src/tokenizer/", "../../src/uncategorized/", "../../src/", "../../src/renderer/", "../../src/scene/", "../../src/ui/", "../../src/resources/", "../../src/utility/" ]
 #precompiled_headers = ["../../src/uncategorized/common.h"]
 precompiled_headers = []
 project_name = "AnKi"
 compiler = "g++"
 common_flags = ""
-compiler_flags = "-c -pedantic-errors -pedantic -ansi -Wall -Wextra -W -pipe -O0 -g3 -pg `sdl-config --cflags` -D_DEBUG_ -D_TERMINAL_COLORING_ -D_PLATFORM_LINUX_ -I../../../glew/include"
+compiler_flags = "-c -pedantic-errors -pedantic -ansi -Wall -Wextra -W -pipe -O0 -g3 -pg `sdl-config --cflags` -D_DEBUG_ -D_TERMINAL_COLORING_ -D_PLATFORM_LINUX_"
 precompiled_headers_flags = "-x c++-header"
-linker_flags = "-lGL -lGLU -lSDL_image `sdl-config --static-libs` -ljpeg -lGLEW -L../../../glew/lib"
+linker_flags = "-lGL -lGLU -lSDL_image `sdl-config --static-libs` -ljpeg -lGLEW"
