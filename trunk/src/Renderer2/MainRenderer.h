17916b6002ebd6971c7c65a9f595b54447f02583
godlike_panos <godlike_panos@cf34f53d-7049-02d2-63c7-51c127bf19f8>
2010-05-25 16:00:42 +0000

    Working on the new renderer

--- trunk/src/Renderer2/MainRenderer.h
+++ trunk/src/Renderer2/MainRenderer.h
@@ -13,6 +13,11 @@ class MainRenderer: public Renderer
 	PROPERTY_RW( int, screenshotJpegQuality, setScreenshotJpegQuality, getScreenshotJpegQuality ) ///< The quality of the JPEG screenshots. From 0 to 100
 	PROPERTY_R( int, maxColorAtachments, getMaxColorAtachments ) ///< Max color attachments a FBO can accept
 
+	/**
+	 * The global rendering quality of the raster image. Its a percentage of the application's window size. From 0.0(low) to 1.0(high)
+	 */
+	PROPERTY_R( float, renderingQuality, getRenderingQuality )
+
 	private:
 		ShaderProg sProg; ///< Final pass' shader program
 
@@ -22,7 +27,12 @@ class MainRenderer: public Renderer
 	public:
 		MainRenderer(): screenshotJpegQuality( 90 ) {}
 
-		void init();
+		void init( const RendererInitializer& initializer );
+
+		/**
+		 * The same as Renderer::render but in addition it renders the final FAI to the framebuffer
+		 * @param cam @see Renderer::render
+		 */
 		void render( Camera& cam );
 		void takeScreenshot( const char* filename ); ///< Save the colorbuffer as 24bit uncompressed TGA image
 };
