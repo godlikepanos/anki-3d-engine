c993364e0fb6c0dc3c6324ad557251a9eda877ce
Panagiotis Christopoulos Charitos <godlikepanos@users.noreply.github.com>
2010-09-06 16:08:11 +0000

    Adding a few interfaces

--- src/Scripting/Math/Vec4.bpi.h
+++ src/Scripting/Math/Vec4.bpi.h
@@ -1,14 +1,15 @@
 
-class_<Vec3>("Vec3")
-	.def_readwrite("x", &Vec3::x)
-	.def_readwrite("y", &Vec3::y)
-	.def_readwrite("z", &Vec3::z)
+class_<Vec4>("Vec4")
+	.def_readwrite("x", &Vec4::x)
+	.def_readwrite("y", &Vec4::y)
+	.def_readwrite("z", &Vec4::z)
+	.def_readwrite("w", &Vec4::w)
 	// constructors
 	.def(init<>())
 	.def(init<float>())
-	.def(init<float, float, float>())
-	.def(init<const Vec2&, float>())
-	.def(init<const Vec3&>())
+	.def(init<float, float, float, float>())
+	.def(init<const Vec2&, float, float>())
+	.def(init<const Vec3&, float>())
 	.def(init<const Vec4&>())
 	.def(init<const Quat&>())
 	// ops with self
