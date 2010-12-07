bae5ebea63d31973a8cd55252bea4b0aadea07e7
Panagiotis Christopoulos Charitos <godlikepanos@users.noreply.github.com>
2010-12-07 00:27:08 +0000

    Small change

--- shaders/Dbg.glsl
+++ shaders/Dbg.glsl
@@ -1,20 +1,26 @@
+///
 #pragma anki vertShaderBegins
 
-#pragma anki attribute position 0
-attribute vec3 position;
+layout(location = 0) in vec3 position;
+layout(location = 1) in vec3 color;
 
 uniform mat4 modelViewProjectionMat;
 
+out vec3 vColor;
+
 void main()
 {
-	gl_Position = modelViewProjectionMat * vec4( position, 1.0 );
+	vColor = color;
+	gl_Position = modelViewProjectionMat * vec4(position, 1.0);
 }
 
 #pragma anki fragShaderBegins
 
-uniform vec4 color = vec4( 0.5 );
+in vec3 vColor;
+
+out vec3 fColor;
 
 void main()
 {
-	gl_FragData[0] = color;
+	fColor = vColor;
 }
