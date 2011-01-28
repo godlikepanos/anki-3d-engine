f063f66011514088520709b6e465e206bfc663e5
godlike@ancient-ritual.com <godlike@ancient-ritual.com@cf34f53d-7049-02d2-63c7-51c127bf19f8>
2011-01-28 00:17:58 +0000

    Visibility determination

--- trunk/src/Scene/VisibilityTester.cpp
+++ trunk/src/Scene/VisibilityTester.cpp
@@ -39,34 +39,6 @@ void VisibilityTester::test(const Camera& cam)
 	spotLights.clear();
 
 	//
-	// Collect the SceneRenderable
-	//
-	Scene::Types<ModelNode>::ConstIterator it = scene.getModelNodes().begin();
-	for(; it != scene.getModelNodes().end(); it++)
-	{
-		boost::ptr_vector<ModelNodePatch>::const_iterator it2 = (*it)->getModelNodePatches().begin();
-		for(; it2 != (*it)->getModelNodePatches().end(); it2++)
-		{
-			const ModelNodePatch& modelNodePatch = *it2;
-
-			if(modelNodePatch.getCpMtl().isBlendingEnabled())
-			{
-				bsRenderables.push_back(&modelNodePatch);
-			}
-			else
-			{
-				msRenderables.push_back(&modelNodePatch);
-			}
-		}
-	}
-
-	//
-	// Sort the renderables from closest to the camera to the farthest
-	//
-	std::sort(msRenderables.begin(), msRenderables.end(), CmpLength(cam.getWorldTransform().origin));
-	std::sort(bsRenderables.begin(), bsRenderables.end(), CmpLength(cam.getWorldTransform().origin));
-
-	//
 	// Collect the lights
 	//
 	Scene::Types<Light>::ConstIterator itl = scene.getLights().begin();
@@ -84,7 +56,8 @@ void VisibilityTester::test(const Camera& cam)
 				Sphere sphere(pointl.getWorldTransform().origin, pointl.getRadius());
 				if(cam.insideFrustum(sphere))
 				{
-					pointLights.push_back(&pointl);
+					pointLights.push_back(VisibleLight<PointLight>());
+					pointLights.back().light = &pointl;
 				}
 				break;
 			}
@@ -95,10 +68,64 @@ void VisibilityTester::test(const Camera& cam)
 
 				if(cam.insideFrustum(spotl.getCamera()))
 				{
-					spotLights.push_back(&spotl);
+					spotLights.push_back(VisibleLight<SpotLight>());
+					spotLights.back().light = &spotl;
 				}
 				break;
 			}
 		}
+	}
+
+	//
+	// Collect the renderables
+	//
+	Scene::Types<ModelNode>::ConstIterator it = scene.getModelNodes().begin();
+	for(; it != scene.getModelNodes().end(); it++)
+	{
+		boost::ptr_vector<ModelNodePatch>::const_iterator it2 = (*it)->getModelNodePatches().begin();
+		for(; it2 != (*it)->getModelNodePatches().end(); it2++)
+		{
+			const ModelNodePatch& modelNodePatch = *it2;
+
+			// First check if its rendered by a light
+			Types<VisibleLight<SpotLight> >::Iterator itsl = spotLights.begin();
+			for(; itsl != spotLights.end(); itsl++)
+			{
+				const SpotLight& spotLight = *(itsl->light);
+				if(test(modelNodePatch, spotLight.getCamera()))
+				{
+					itsl->renderables.push_back(&modelNodePatch);
+				}
+			}
+
+
+			/// @todo Perform real tests
+			if(test(modelNodePatch, cam))
+			{
+				if(modelNodePatch.getCpMtl().isBlendingEnabled())
+				{
+					bsRenderables.push_back(&modelNodePatch);
+				}
+				else
+				{
+					msRenderables.push_back(&modelNodePatch);
+				}
+			}
+		}
+	}
+
+	//
+	// Sort the renderables from closest to the camera to the farthest
+	//
+	std::sort(msRenderables.begin(), msRenderables.end(), CmpLength(cam.getWorldTransform().origin));
+	std::sort(bsRenderables.begin(), bsRenderables.end(), CmpLength(cam.getWorldTransform().origin));
+
+	//
+	// Short the light's renderables
+	//
+	Types<VisibleLight<SpotLight> >::Iterator itsl = spotLights.begin();
+	for(; itsl != spotLights.end(); itsl++)
+	{
+		std::sort(itsl->renderables.begin(), itsl->renderables.end(), CmpLength(itsl->light->getWorldTransform().origin));
 	}
 }
--- trunk/src/Scene/VisibilityTester.h
+++ trunk/src/Scene/VisibilityTester.h
@@ -16,15 +16,33 @@ class PointLight;
 /// Performs visibility determination tests and fills a few containers with the visible scene nodes
 class VisibilityTester
 {
+	//====================================================================================================================
+	// Public                                                                                                            =
+	//====================================================================================================================
 	public:
 		/// Types
 		template<typename Type>
 		class Types
 		{
 			public:
-				typedef std::deque<const Type*> Container;
+				typedef std::deque<Type> Container;
 				typedef typename Container::iterator Iterator;
 				typedef typename Container::const_iterator ConstIterator;
+		};
+
+		/// The information about the visible lights
+		template<typename LightType>
+		class VisibleLight
+		{
+			friend class VisibilityTester;
+
+			public:
+				const LightType& getLight() const {return *light;}
+				GETTER_R(Types<const SceneRenderable*>::Container, renderables, getRenderables)
+
+			private:
+				const LightType* light;
+				Types<const SceneRenderable*>::Container renderables; ///< The visible nodes by that light
 		};
 
 		/// Constructor
@@ -32,14 +50,17 @@ class VisibilityTester
 
 		/// @name Accessors
 		/// @{
-		GETTER_R(Types<SceneRenderable>::Container, msRenderables, getMsRenderables)
-		GETTER_R(Types<SceneRenderable>::Container, bsRenderables, getBsRenderables)
-		GETTER_R(Types<PointLight>::Container, pointLights, getPointLights)
-		GETTER_R(Types<SpotLight>::Container, spotLights, getSpotLights)
+		GETTER_R(Types<const SceneRenderable*>::Container, msRenderables, getMsRenderables)
+		GETTER_R(Types<const SceneRenderable*>::Container, bsRenderables, getBsRenderables)
+		GETTER_R(Types<VisibleLight<PointLight> >::Container, pointLights, getPointLights)
+		GETTER_R(Types<VisibleLight<SpotLight> >::Container, spotLights, getSpotLights)
 		/// @}
 
 		void test(const Camera& cam);
 
+	//====================================================================================================================
+	// Private                                                                                                           =
+	//====================================================================================================================
 	private:
 		/// Used in sorting. Compare the length of 2 nodes from the camera
 		struct CmpLength
@@ -51,10 +72,13 @@ class VisibilityTester
 
 		const Scene& scene; ///< Know your father
 
-		Types<SceneRenderable>::Container msRenderables;
-		Types<SceneRenderable>::Container bsRenderables;
-		Types<PointLight>::Container pointLights;
-		Types<SpotLight>::Container spotLights;
+		Types<const SceneRenderable*>::Container msRenderables;
+		Types<const SceneRenderable*>::Container bsRenderables;
+		Types<VisibleLight<PointLight> >::Container pointLights;
+		Types<VisibleLight<SpotLight> >::Container spotLights;
+
+		/// @todo write some real code
+		static bool test(const SceneRenderable& /*renderable*/, const Camera& /*cam*/) {return true;}
 };
 
 
