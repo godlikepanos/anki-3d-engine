e444dd9e333833a1c6eec7b179c549ea1cca00ae
Panagiotis Christopoulos Charitos <godlikepanos@users.noreply.github.com>
2011-03-15 23:53:26 +0000

    no log

--- src/Scene/MaterialRuntime/MaterialRuntime.h
+++ src/Scene/MaterialRuntime/MaterialRuntime.h
@@ -3,6 +3,8 @@
 
 #include <boost/ptr_container/ptr_vector.hpp>
 #include "MtlUserDefinedVarRuntime.h"
+#include "Properties.h"
+#include "CharPtrHashMap.h"
 
 
 class Material;
@@ -14,9 +16,24 @@ class MaterialRuntime
 	public:
 		MaterialRuntime(const Material& mtl);
 
+		/// @name Accessors
+		/// @{
+		GETTER_RW(boost::ptr_vector<MtlUserDefinedVarRuntime>, userDefVars, getUserDefVars)
+
+		/// Find MtlUserDefinedVarRuntime variable. On failure it throws an exception
+		/// @param[in] name The name of the var
+		/// @return It returns a MtlUserDefinedVarRuntime
+		/// @exception Exception
+		MtlUserDefinedVarRuntime& getUserDefinedVarByName(const char* name);
+
+		/// @see getUserDefinedVarByName
+		const MtlUserDefinedVarRuntime& getUserDefinedVarByName(const char* name) const;
+		/// @}
+
 	private:
 		const Material& mtl; ///< The resource
 		boost::ptr_vector<MtlUserDefinedVarRuntime> userDefVars;
+		CharPtrHashMap<MtlUserDefinedVarRuntime*> userDefVarsHashMap; ///< For fast finding the variables
 };
 
 
