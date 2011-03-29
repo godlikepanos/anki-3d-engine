68b703af4f59b2c30317dd8b7b4b3bd97d28da7f
godlike@ancient-ritual.com <godlike@ancient-ritual.com@cf34f53d-7049-02d2-63c7-51c127bf19f8>
2011-03-29 22:37:30 +0000

    - refactoring - python

--- trunk/src/Scene/MaterialRuntime/MaterialRuntimeUserDefinedVar.cpp
+++ trunk/src/Scene/MaterialRuntime/MaterialRuntimeUserDefinedVar.cpp
@@ -1,36 +1,36 @@
-#include "MtlUserDefinedVarRuntime.h"
+#include "MaterialRuntimeUserDefinedVar.h"
 
 
 //======================================================================================================================
 // Visitor functors                                                                                                    =
 //======================================================================================================================
 
-void MtlUserDefinedVarRuntime::ConstructVisitor::operator()(float x) const
+void MaterialRuntimeUserDefinedVar::ConstructVisitor::operator()(float x) const
 {
 	udvr.data = x;
 }
 
-void MtlUserDefinedVarRuntime::ConstructVisitor::operator()(const Vec2& x) const
+void MaterialRuntimeUserDefinedVar::ConstructVisitor::operator()(const Vec2& x) const
 {
 	udvr.data = x;
 }
 
-void MtlUserDefinedVarRuntime::ConstructVisitor::operator()(const Vec3& x) const
+void MaterialRuntimeUserDefinedVar::ConstructVisitor::operator()(const Vec3& x) const
 {
 	udvr.data = x;
 }
 
-void MtlUserDefinedVarRuntime::ConstructVisitor::operator()(const Vec4& x) const
+void MaterialRuntimeUserDefinedVar::ConstructVisitor::operator()(const Vec4& x) const
 {
 	udvr.data = x;
 }
 
-void MtlUserDefinedVarRuntime::ConstructVisitor::operator()(const RsrcPtr<Texture>& x) const
+void MaterialRuntimeUserDefinedVar::ConstructVisitor::operator()(const RsrcPtr<Texture>& x) const
 {
 	udvr.data = &x;
 }
 
-void MtlUserDefinedVarRuntime::ConstructVisitor::operator()(MtlUserDefinedVar::Fai x) const
+void MaterialRuntimeUserDefinedVar::ConstructVisitor::operator()(MtlUserDefinedVar::Fai x) const
 {
 	udvr.data = x;
 }
@@ -39,7 +39,7 @@ void MtlUserDefinedVarRuntime::ConstructVisitor::operator()(MtlUserDefinedVar::F
 //======================================================================================================================
 // Constructor                                                                                                         =
 //======================================================================================================================
-MtlUserDefinedVarRuntime::MtlUserDefinedVarRuntime(const MtlUserDefinedVar& rsrc_):
+MaterialRuntimeUserDefinedVar::MaterialRuntimeUserDefinedVar(const MtlUserDefinedVar& rsrc_):
 	rsrc(rsrc_)
 {
 	// Initialize the data using a visitor
