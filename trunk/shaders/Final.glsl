7a8eb9193482a273567d85591d92ec0023f4d12d
godlike@ancient-ritual.com <godlike@ancient-ritual.com@cf34f53d-7049-02d2-63c7-51c127bf19f8>
2011-04-07 22:51:29 +0000

    SSAO

--- trunk/shaders/Final.glsl
+++ trunk/shaders/Final.glsl
@@ -14,17 +14,8 @@ void main()
 {
 	//if( gl_FragCoord.x > 0.5 ) discard;
 
-	fFragColor = texture2D(rasterImage, vTexCoords).rgb;
+	//fFragColor = texture2D(rasterImage, vTexCoords).rgb;
 	
-	//fragColor = vec3(1.0, 0.0, 1.0);
-
-
-	//gl_FragColor.rgb = vec3(texture2D(rasterImage, vTexCoords).r);
-	/*vec4 c = texture2D( rasterImage, texCoords );
-	if( c.r == 0.0 && c.g == 0.0 && c.b==0.0 && c.a != 0.0 )*/
-		//gl_FragColor.rgb = vec3( texture2D( rasterImage, texCoords ).a );
-	//gl_FragColor.rgb = MedianFilter( rasterImage, texCoords );
-	//gl_FragColor.rgb = vec3( gl_FragCoord.xy/tex_size_, 0.0 );
-	//gl_FragColor.rgb = vec3( gl_FragCoord.xy*vec2( 1.0/R_W, 1.0/R_H ), 0.0 );
-	//gl_FragColor.rgb = texture2D( rasterImage, gl_FragCoord.xy/textureSize(rasterImage,0) ).rgb;
+	fFragColor = vec3(texture2D(rasterImage, vTexCoords).r);
+	
 }
