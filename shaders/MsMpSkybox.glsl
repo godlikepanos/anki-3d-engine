b5a28738f824321268c0c1b98ae5c8af57c40353
Panagiotis Christopoulos Charitos <godlikepanos@users.noreply.github.com>
2011-04-14 15:50:24 +0000

    - Adding a new switch in MsMpGeneric - Removing obsolete file - Refactoring

--- shaders/MsMpSkybox.glsl
+++ shaders/MsMpSkybox.glsl
@@ -1,19 +1,19 @@
 #pragma anki vertShaderBegins
 
-attribute vec3 position;
-attribute vec2 texCoords;
+in vec3 position;
+in vec2 texCoords;
 
 uniform mat4 viewProjectionMat;
 uniform mat4 viewMat;
 
-varying vec2 texCoordsV2f;
+out vec2 vTexCoords;
 
 void main()
 {
-	texCoordsV2f = texCoords;
+	vTexCoords = texCoords;
 	
 	mat4 modelMat = viewMat;
-	modelMat[3] = modelMat[7] = modelMat[11] = 0.0; // clear the tranlation. We want only the rotation part
+	modelMat[3] = modelMat[7] = modelMat[11] = 0.0; // clear the translation part. We want only the rotation part
 	
 	gl_Position = viewProjectionMat * (modelMat * vec4(position, 1.0));
 }
@@ -25,18 +25,20 @@ uniform sampler2D noiseMap;
 uniform float timer = 0.0;
 uniform vec3 sceneAmbientCol;
 
-varying vec2 texCoordsV2f;
+in vec2 vTexCoords;
+
+layout(location = 1) out vec3 fMsDiffuseFai;
 
 #define PI 3.14159265358979323846
 
 
 void main()
 {
-	float scaledTimer = timer*0.4;
+	float scaledTimer = timer * 0.4;
 
-	vec2 displacement = texCoordsV2f - vec2(scaledTimer);
+	vec2 displacement = vTexCoords - vec2(scaledTimer);
 
-	vec3 noiseVec(normalize(texture2D(noisemap, displacement).xyz));
+	vec3 noiseVec = normalize(texture2D(noisemap, displacement).xyz);
 	noiseVec = (noiseVec * 2.0 - 1.0);
 	
 	/*
@@ -44,13 +46,13 @@ void main()
 	 * and one in the middle of the quad. We use it in order to reduse the distortion at the polygon edges because of
 	 * artifacts with the neighbor polygons
 	 */
-	float edgeFactor = sin(texCoordsV2f.s * PI) * sin(texCoordsV2f.t * PI);
+	float edgeFactor = sin(vTexCoords.s * PI) * sin(vTexCoords.t * PI);
 
 	const float strengthFactor = 0.015;
 
 	noiseVec = noiseVec * strengthFactor * edgeFactor;
 
 	// write to the diffuse buffer
-	gl_FragData[1].rgb = texture2D(colormap, texCoordsV2f + noiseVec.xy).rgb * sceneAmbientCol; 
+	fMsDiffuseFai = texture2D(colormap, vTexCoords + noiseVec.xy).rgb * sceneAmbientCol;
 }
 
