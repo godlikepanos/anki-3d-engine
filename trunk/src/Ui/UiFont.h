0cca09d3d41eeda66f912350994231dddbc9864c
godlike@ancient-ritual.com <godlike@ancient-ritual.com@cf34f53d-7049-02d2-63c7-51c127bf19f8>
2011-05-23 16:15:41 +0000

    Font

--- trunk/src/Ui/UiFont.h
+++ trunk/src/Ui/UiFont.h
@@ -2,7 +2,9 @@
 #define UI_FONT_H
 
 #include <memory>
+#include <boost/ptr_container/ptr_vector.hpp>
 #include "StdTypes.h"
+#include "Math.h"
 
 
 class Texture;
@@ -18,13 +20,16 @@ class Font
 		Font(const char* fontFilename, uint nominalWidth, uint NominalHeight);
 
 	private:
-		/// @todo
-		struct FtGlyph
+		struct Glyph
 		{
-
+			/// Transforms the default texture coordinates to the glyph's. The default are (1, 1), (0, 1), (0, 0),
+			/// (0, 1)
+			Mat3 textureMat;
+			float horizontalAdvance;
 		};
 
-		std::auto_ptr<Texture> map;
+		std::auto_ptr<Texture> map; ///< The texture map that contains all the glyphs
+		boost::ptr_vector<Glyph> glyphs; ///< A set of glyphs from ' ' to ' ' + 128
 
 
 };
