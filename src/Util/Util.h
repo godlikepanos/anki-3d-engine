23886554e17193bbf2bba74d230d6d0de71910ab
Panagiotis Christopoulos Charitos <godlikepanos@users.noreply.github.com>
2011-07-21 17:22:19 +0000

    Refactoring

--- src/Util/Util.h
+++ src/Util/Util.h
@@ -9,14 +9,26 @@
 /// The namespace contains a few useful functions
 namespace Util {
 
-extern int randRange(int min, int max); ///< Pick a random number from min to max
-extern uint randRange(uint min, uint max); ///< Pick a random number from min to max
-extern float randRange(float min, float max); ///< Pick a random number from min to max
-extern double randRange(double min, double max); ///< Pick a random number from min to max
+/// Pick a random number from min to max
+extern int randRange(int min, int max);
+
+/// Pick a random number from min to max
+extern uint randRange(uint min, uint max);
+
+/// Pick a random number from min to max
+extern float randRange(float min, float max);
+
+/// Pick a random number from min to max
+extern double randRange(double min, double max);
+
 extern float randFloat(float max);
 
-extern std::string readFile(const char* filename); ///< Open a text file and return its contents into a string
-extern Vec<std::string> getFileLines(const char* filename); ///< Open a text file and return its lines into a string vector
+/// Open a text file and return its contents into a string
+extern std::string readFile(const char* filename);
+
+/// Open a text file and return its lines into a string vector
+extern Vec<std::string> getFileLines(const char* filename);
+
 
 }
 
