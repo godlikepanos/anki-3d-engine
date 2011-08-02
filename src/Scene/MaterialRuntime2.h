db5225c1742ece3a28ab9b147288ae195f61d8de
Panagiotis Christopoulos Charitos <godlikepanos@users.noreply.github.com>
2011-08-02 16:21:17 +0000

    Material

--- src/Scene/MaterialRuntime2.h
+++ src/Scene/MaterialRuntime2.h
@@ -2,11 +2,34 @@
 #define MATERIAL_RUNTIME_2_H
 
 #include "Resources/Material.h"
+#include "Util/Accessors.h"
+#include <boost/ptr_container/ptr_vector.hpp>
 
 
-/// XXX
+class UserMaterialVariableRuntime;
+
+
+/// One layer above material resource
 class MaterialRuntime2: private MaterialProps
 {
+	public:
+		/// A type
+		typedef boost::ptr_vector<UserMaterialVariableRuntime>
+			VariablesContainer;
+
+		/// @name Constructors & destructors
+		/// @{
+		MaterialRuntime2(const Material2& mtl);
+		~MaterialRuntime2();
+		/// @}
+
+		/// @name Accessors
+		/// @{
+		GETTER_RW(VariablesContainer, vars, getUserMaterialVariablesRuntime)
+		/// @}
+
+	private:
+		VariablesContainer vars;
 };
 
 
--- src/Scene/UserMaterialVariableRuntime.h
+++ src/Scene/UserMaterialVariableRuntime.h
@@ -3,6 +3,7 @@
 
 #include "Util/Accessors.h"
 #include "Math/Math.h"
+#include <boost/variant.hpp>
 
 
 class UserMaterialVariable;
@@ -13,6 +14,10 @@ class Texture;
 class UserMaterialVariableRuntime
 {
 	public:
+		/// The data union. The Texture resource is read-only at runtime
+		typedef boost::variant<float, Vec2, Vec3, Vec4,
+			const Texture*> DataVariant;
+
 		/// Constructor
 		UserMaterialVariableRuntime(const UserMaterialVariable& umv);
 
@@ -20,26 +25,11 @@ class UserMaterialVariableRuntime
 		/// @{
 		GETTER_R(UserMaterialVariable, umv, getUserMaterialVariable)
 
-		GETTER_SETTER_BY_VAL(float, data.scalar, getFloat, setFloat)
-		GETTER_SETTER(Vec2, data.vec2, getVec2, setVec2)
-		GETTER_SETTER(Vec3, data.vec3, getVec3, setVec3)
-		GETTER_SETTER(Vec4, data.vec4, getVec4, setVec4)
-		const Texture& getTexture() const {return *data.tex;}
 		/// @}
 
 	private:
-		/// XXX
-		struct Data
-		{
-			float scalar;
-			Vec2 vec2;
-			Vec3 vec3;
-			Vec4 vec4;
-			const Texture* tex;
-		};
-
 		const UserMaterialVariable& umv; ///< Know the resource
-		Data data;
+		DataVariant data;
 };
 
 
