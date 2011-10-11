daaab24586ffaf9fde8e10c95a69a56e6fde9460
godlike@ancient-ritual.com <godlike@ancient-ritual.com@cf34f53d-7049-02d2-63c7-51c127bf19f8>
2011-10-11 09:52:11 +0000

    removing externals

--- trunk/extern/download-externs.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-set -x
-cd ../../
-svn checkout http://bullet.googlecode.com/svn/trunk/ bullet
-hg clone http://hg.libsdl.org/SDL SDL
-svn checkout https://glew.svn.sourceforge.net/svnroot/glew/trunk/glew glew
--- trunk/extern/include/SDL/SDL_revision.h
+++
@@ -1,1 +0,0 @@
-#define SDL_REVISION "hg-5313:7a569d09a682"
