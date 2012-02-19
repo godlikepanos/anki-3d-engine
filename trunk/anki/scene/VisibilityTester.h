97e9f49dfcde91bb76b62c71ae82093fc210e7f8
godlike@ancient-ritual.com <godlike@ancient-ritual.com@cf34f53d-7049-02d2-63c7-51c127bf19f8>
2012-02-19 23:31:11 +0000

    Scene

--- trunk/anki/scene/VisibilityTester.h
+++ trunk/anki/scene/VisibilityTester.h
@@ -1,10 +1,8 @@
 #ifndef ANKI_SCENE_VISIBILITY_TESTER_H
 #define ANKI_SCENE_VISIBILITY_TESTER_H
 
-#include "anki/math/Math.h"
-#include "anki/core/ParallelJob.h"
-#include <deque>
-#include <boost/thread.hpp>
+#include <vector>
+#include <boost/range/iterator_range.hpp>
 
 
 namespace anki {
@@ -13,76 +11,51 @@ namespace anki {
 class Camera;
 class Scene;
 class Renderable;
-class SpotLight;
-class PointLight;
-class SceneNode;
-class VisibilityInfo;
+class Frustumable;
+
+
+/// XXX
+class VisibilityInfo
+{
+	friend class VisibilityTester;
+
+public:
+	typedef std::vector<Renderable*> Renderables;
+	typedef std::vector<Frustumable*> Frustumables;
+
+	boost::iterator_range<Renderables::iterator> getRenderables()
+	{
+		return boost::iterator_range<Renderables::iterator>(
+			renderables.begin(), renderables.end());
+	}
+
+	boost::iterator_range<Frustumables::iterator> getFrustumable()
+	{
+		return boost::iterator_range<Frustumables::iterator>(
+			frustumables.begin(), frustumables.end());
+	}
+
+private:
+	Renderables renderables;
+	std::vector<Frustumable*> frustumables;
+};
 
 
 /// Performs visibility determination tests and fills a few containers with the
 /// visible scene nodes
 class VisibilityTester
 {
-	public:
-		/// Constructor
-		VisibilityTester(Scene& scene);
-		~VisibilityTester();
+public:
+	/// Constructor
+	VisibilityTester()
+	{}
 
-		/// This method:
-		/// - Gets the visible renderable nodes
-		/// - Sort them from the closest to the farthest
-		/// - Get the visible lights
-		/// - For every spot light that casts shadow get the visible renderables
-		void test(Camera& cam);
+	~VisibilityTester();
 
-	private:
-		/// Used in sorting. Compare the length of 2 nodes from the camera
-		struct CmpDistanceFromOrigin
-		{
-			Vec3 o; ///< The camera origin
-			CmpDistanceFromOrigin(const Vec3& o_): o(o_) {}
-			bool operator()(const SceneNode* a, const SceneNode* b) const;
-		};
-
-		/// The JobParameters that we feed in the ParallelManager
-		struct VisJobParameters: ParallelJobParameters
-		{
-			const Camera* cam;
-			bool skipShadowless;
-			VisibilityInfo* visibilityInfo;
-			Scene* scene;
-			boost::mutex* msRenderableNodesMtx;
-			boost::mutex* bsRenderableNodesMtx;
-		};
-
-		Scene& scene; ///< Know your father
-
-		/// @name Needed by getRenderableNodesJobCallback
-		/// The vars of this group are needed by the static
-		/// getRenderableNodesJobCallback and kept here so it can access them
-		/// @{
-		boost::mutex msRenderableNodesMtx;
-		boost::mutex bsRenderableNodesMtx;
-		/// @}
-
-		/// Test a node against the camera frustum
-		template<typename Type>
-		static bool test(const Type& tested, const Camera& cam);
-
-		/// Get renderable nodes for a given camera
-		/// @param[in] skipShadowless Skip shadowless nodes. If the cam is a
-		/// light cam
-		/// @param[in] cam The camera to test and gather renderable nodes
-		/// @param[out] storage The VisibilityInfo of where we will store the
-		/// visible nodes
-		void getRenderableNodes(bool skipShadowless, const Camera& cam,
-			VisibilityInfo& storage);
-
-		/// This static method will be fed into the ParallelManager
-		/// @param data This is actually a pointer to VisibilityTester
-		static void getRenderableNodesJobCallback(
-			ParallelJobParameters& data,
-			const ParallelJob& job);
+	/// This method:
+	/// - Gets the visible renderables and frustumables
+	/// - For every frustumable perform tests
+	void test(Frustumable& cam, Scene& scene, VisibilityInfo& vinfo);
 };
 
 
