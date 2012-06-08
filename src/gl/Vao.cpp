8968e888d0774a82c5f823ee416d6bb24cd2bef9
Panagiotis Christopoulos Charitos <godlikepanos@users.noreply.github.com>
2012-06-08 18:32:34 +0000

    Minor changes

--- src/gl/Vao.cpp
+++ src/gl/Vao.cpp
@@ -19,47 +19,34 @@ Vao::~Vao()
 }
 
 //==============================================================================
-void Vao::attachArrayBufferVbo(const Vbo& vbo, GLuint attribVarLocation,
-	GLint size, GLenum type, GLboolean normalized, GLsizei stride,
-	const GLvoid* pointer)
+void Vao::attachArrayBufferVbo(const VaoDescriptor& descr)
 {
 	ANKI_ASSERT(isCreated());
-	if(vbo.getBufferTarget() != GL_ARRAY_BUFFER)
-	{
-		throw ANKI_EXCEPTION("Only GL_ARRAY_BUFFER is accepted");
-	}
+	ANKI_ASSERT(descr.vbo != nullptr);
+	ANKI_ASSERT(descr.attribVar != nullptr);
+	ANKI_ASSERT(descr.size != 0);
+	ANKI_ASSERT(descr.type != 0);
 
-	ANKI_CHECK_GL_ERROR();
+	ANKI_ASSERT(descr.vbo->getBufferTarget() == GL_ARRAY_BUFFER
+		&& "Only GL_ARRAY_BUFFER is accepted");
 
 	bind();
-	vbo.bind();
-	glVertexAttribPointer(attribVarLocation, size, type, normalized,
-		stride, pointer);
-	glEnableVertexAttribArray(attribVarLocation);
-	vbo.unbind();
+	descr.vbo->bind();
+	glVertexAttribPointer(descr.attribVar->getLocation(), descr.size, 
+		descr.type, descr.normalized, descr.stride, descr.pointer);
+	glEnableVertexAttribArray(descr.attribVar->getLocation());
+	descr.vbo->unbind();
 	unbind();
 
 	ANKI_CHECK_GL_ERROR();
 }
 
 //==============================================================================
-void Vao::attachArrayBufferVbo(const Vbo& vbo,
-	const ShaderProgramAttributeVariable& attribVar,
-	GLint size, GLenum type, GLboolean normalized, GLsizei stride,
-	const GLvoid* pointer)
-{
-	attachArrayBufferVbo(vbo, attribVar.getLocation(), size, type, normalized,
-		stride, pointer);
-}
-
-//==============================================================================
 void Vao::attachElementArrayBufferVbo(const Vbo& vbo)
 {
 	ANKI_ASSERT(isCreated());
-	if(vbo.getBufferTarget() != GL_ELEMENT_ARRAY_BUFFER)
-	{
-		throw ANKI_EXCEPTION("Only GL_ELEMENT_ARRAY_BUFFER is accepted");
-	}
+	ANKI_ASSERT(vbo.getBufferTarget() == GL_ELEMENT_ARRAY_BUFFER
+		&& "Only GL_ELEMENT_ARRAY_BUFFER is accepted");
 
 	bind();
 	vbo.bind();
