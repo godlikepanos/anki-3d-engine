33190b44b6ce938f5781a71c0f550e91d9ff7b53
godlike@ancient-ritual.com <godlike@ancient-ritual.com@cf34f53d-7049-02d2-63c7-51c127bf19f8>
2012-07-25 17:25:47 +0000

    SMO

--- trunk/include/anki/renderer/Smo.h
+++ trunk/include/anki/renderer/Smo.h
@@ -2,10 +2,15 @@
 #define ANKI_RENDERER_SMO_H
 
 #include "anki/renderer/RenderingPass.h"
+#include "anki/resource/Resource.h"
+#include "anki/gl/Vao.h"
+#include "anki/scene/Camera.h"
+#include <array>
 
 namespace anki {
 
-class Light;
+class PointLight;
+class SpotLight;
 
 /// Stencil masking optimizations
 class Smo: public RenderingPass
@@ -18,11 +23,32 @@ public:
 	~Smo();
 
 	void init(const RendererInitializer& initializer);
-	void run(const Light& light);
+	void run(const PointLight& light);
+	void run(const SpotLight& light);
 
 private:
+	/// @todo
+	struct Geom
+	{
+		Geom();
+		~Geom();
+
+		MeshResourcePointer mesh;
+		Vao vao;
+	};
+
+	Geom sphereGeom;
+
+	/// An array of geometry stuff. For perspective cameras the shape is a
+	/// pyramid, see the blend file with the vertex positions
+	std::array<Geom, Camera::CT_COUNT> camGeom;
+
+	ShaderProgramResourcePointer sProg;
+
+	void setupGl(bool inside);
+	void restoreGl(bool inside);
 };
 
-} // end namespace
+} // end namespace anki
 
 #endif
