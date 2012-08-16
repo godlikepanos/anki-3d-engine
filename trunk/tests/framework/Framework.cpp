c3b49d4abb7b2ee5da19a19f65fa8aa8de7f9a27
godlike@ancient-ritual.com <godlike@ancient-ritual.com@cf34f53d-7049-02d2-63c7-51c127bf19f8>
2012-08-16 16:00:41 +0000

    Tests

--- trunk/tests/framework/Framework.cpp
+++ trunk/tests/framework/Framework.cpp
@@ -49,6 +49,77 @@ void Tester::addTest(const char* name, const char* suiteName,
 }
 
 //==============================================================================
+int Tester::run(int argc, char** argv)
+{
+	const char* helpMessage = ""
+
+	programName = argv[0];
+
+	// Parse args
+	std::string suite;
+	std::string test;
+
+	for(int i = 1; i < argc; i++)
+	{
+		const char* arg = args[i];
+		if(strcmp(arg, "--list-tests") == 0)
+		{
+			listTests();
+			return 0;
+		}
+		else if(strcmp(arg, "--help") == 0)
+		{
+			// XXX
+		}
+		else if(strcmp(arg, "--suite") == 0)
+		{
+			if(i + 1 >= argc)
+			{
+				// XXX
+			}
+			suite = arg;
+			++i;
+		}
+		else if(strcmp(arg, "--test") == 0)
+		{
+			if(i + 1 >= argc)
+			{
+				// XXX
+			}
+			test = arg;
+			++i;
+		}
+	}
+
+	// Run tests
+	try
+	{
+		if(argc == 1)
+		{
+			// Run all
+			for(TestSuite* suite : suites)
+			{
+				for(Test* test : suite->tests)
+				{
+					test->callback(*test);
+				}
+			}
+		}
+		else
+		{
+			// Run some
+		}
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+		return 1;
+	}
+
+	return 0;
+}
+
+//==============================================================================
 int Tester::runAll()
 {
 	int errors = 0;
@@ -61,6 +132,21 @@ int Tester::runAll()
 	}
 
 	return errors;
+}
+
+//==============================================================================
+int Tester::listTests()
+{
+	for(TestSuite* suite : suites)
+	{
+		for(Test* test : suite->tests)
+		{
+			std::cout << "./" << args[0] << " --suite " << suite->name 
+				<< " --test " << test->name << std::endl;
+		}
+	}
+
+	return 0;
 }
 
 } // end namespace anki
