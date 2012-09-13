d63cccf7f147958e20e3093d87f1e3e70f82e918
Panagiotis Christopoulos Charitos <godlikepanos@users.noreply.github.com>
2012-09-13 16:28:45 +0000

    IS

--- shaders/Pack.glsl
+++ shaders/Pack.glsl
@@ -22,6 +22,8 @@ vec3 unpackNormal(in vec2 enc)
 #define MAX_SPECULARITY 128.0
 
 /// Pack specular stuff
+/// @param c The specular component. c.x is the color in grayscale and c.y the 
+///          specularity
 float packSpecular(in vec2 c)
 {
 	return round(c[0] * 15.0) * 16.0 / 255.0 
@@ -31,11 +33,12 @@ float packSpecular(in vec2 c)
 /// Unpack specular
 vec2 unpackSpecular(in float f)
 {
-	float r = floor(f * 255.0 / 16.0);
+	float r = floor(f * (255.0 / 16.0));
 
 	return vec2(
 		r / 15.0,
-		f * 255.0 * MAX_SPECULARITY / 15.0 - r * 16.0 * MAX_SPECULARITY / 15.0);
+		f * (255.0 * MAX_SPECULARITY / 15.0) 
+		- r * (16.0 * MAX_SPECULARITY / 15.0));
 }
 
 /// Pack a vec4 to float
@@ -43,8 +46,8 @@ vec2 unpackSpecular(in float f)
 float pack4x8ToFloat(in vec4 v)
 {
 	const vec4 unshift = vec4(
-		1.0 / (256.0f * 256.0 * 256.0), 
-		1.0f / (256.0 * 256.0), 
+		1.0 / (256.0 * 256.0 * 256.0), 
+		1.0 / (256.0 * 256.0), 
 		1.0 / 256.0, 1.0);
 	return dot(v, unshift);
 }
