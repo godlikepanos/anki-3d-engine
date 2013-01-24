f7fca51e805828b803d1f8eb5df6c894c5ad23ea
Panagiotis Christopoulos Charitos <godlikepanos@users.noreply.github.com>
2013-01-24 22:13:54 +0000

    Memory stuff

--- tests/util/Memory.cpp
+++ tests/util/Memory.cpp
@@ -1,18 +1,34 @@
 #include "tests/framework/Framework.h"
 #include "tests/util/Foo.h"
 #include "anki/util/Memory.h"
+#include <type_traits>
 
 ANKI_TEST(Memory, Test)
 {
-	/*constexpr U n = 4;
+	constexpr U n = 4;
 	StackAllocator<U8, true> alloc(sizeof(Foo) * n + sizeof(PtrSize));
 
-	Foo* f = New<Foo, StackAllocator<Foo, true>>(n, alloc)();
+	Foo* f = ANKI_NEW(Foo, alloc, 123);
 
-	ANKI_TEST_EXPECT_EQ(alloc.dump(), false);
+	ANKI_TEST_EXPECT_EQ(f->x, 123);
 
-	DeleteArray<Foo, StackAllocator<Foo, true>>{alloc}(f);
+	ANKI_TEST_EXPECT_EQ(alloc.getMemoryPool().getAllocatedSize(),
+		sizeof(Foo) + sizeof(U32));
 
-	ANKI_TEST_EXPECT_EQ(alloc.dump(), true);*/
+	ANKI_DELETE(f, alloc);
+
+	ANKI_TEST_EXPECT_EQ(alloc.getMemoryPool().getAllocatedSize(), 0);
+
+	// Array
+	f = ANKI_NEW_ARRAY(Foo, alloc, 2, 123);
+
+	ANKI_TEST_EXPECT_EQ(alloc.getMemoryPool().getAllocatedSize(),
+		2 * sizeof(Foo) + sizeof(U32) + sizeof(PtrSize));
+
+	ANKI_TEST_EXPECT_NEQ(alloc.getMemoryPool().getAllocatedSize(), 0);
+
+	ANKI_DELETE_ARRAY(f, alloc);
+
+	ANKI_TEST_EXPECT_EQ(alloc.getMemoryPool().getAllocatedSize(), 0);
 }
 
