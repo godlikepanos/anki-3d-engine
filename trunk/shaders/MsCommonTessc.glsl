bf6a7f31721c63d1deb0c23fbc856d7babf93d80
godlike@ancient-ritual.com <godlike@ancient-ritual.com@cf34f53d-7049-02d2-63c7-51c127bf19f8>
2013-10-10 19:54:20 +0000

    Updating with a subdiv algorithm. It seems too expensive

--- trunk/shaders/MsCommonTessc.glsl
+++ trunk/shaders/MsCommonTessc.glsl
@@ -1,15 +1,16 @@
 #pragma anki include "shaders/MsBsCommon.glsl"
 
-layout(vertices = 3) out;
+layout(vertices = 1) out;
 
 // Varyings in
 in highp vec3 vPosition[];
 in highp vec2 vTexCoords[];
-#if PASS_COLOR
 in mediump vec3 vNormal[];
+#if PASS_COLOR
 in mediump vec4 vTangent[];
 #endif
 
+#if 0
 // Varyings out
 out highp vec3 tcPosition[];
 out highp vec2 tcTexCoords[];
@@ -17,25 +18,100 @@ out highp vec2 tcTexCoords[];
 out mediump vec3 tcNormal[];
 out mediump vec4 tcTangent[];
 #endif
+#endif
+
+struct OutPatch
+{
+	vec3 pos030;
+	vec3 pos021;
+	vec3 pos012;
+	vec3 pos003;
+	vec3 pos102;
+	vec3 pos201;
+	vec3 pos300;
+	vec3 pos210;
+	vec3 pos120;
+	vec3 pos111;
+
+	vec2 texCoord[3];
+	vec3 normal[3];
+#if PASS_COLOR
+	vec4 tangent[3];
+#endif
+};
+
+out patch OutPatch tcPatch;
+
+// Project point to plane
+vec3 projectToPlane(vec3 point, vec3 planePoint, vec3 planeNormal)
+{
+	vec3 v = point - planePoint;
+	float pen = dot(v, planeNormal);
+	vec3 d = pen * planeNormal;
+	return (point - d);
+} 
+
+// Calculate control points
+void calcPositions()
+{
+	// The original vertices stay the same
+	tcPatch.pos030 = vPosition[0];
+	tcPatch.pos003 = vPosition[1];
+	tcPatch.pos300 = vPosition[2];
+
+	// edges are names according to the opposing vertex
+	vec3 edgeB300 = tcPatch.pos003 - tcPatch.pos030;
+	vec3 edgeB030 = tcPatch.pos300 - tcPatch.pos003;
+	vec3 edgeB003 = tcPatch.pos030 - tcPatch.pos300;
+
+	// Generate two midpoints on each edge
+	tcPatch.pos021 = tcPatch.pos030 + edgeB300 / 3.0;
+	tcPatch.pos012 = tcPatch.pos030 + edgeB300 * 2.0 / 3.0;
+	tcPatch.pos102 = tcPatch.pos003 + edgeB030 / 3.0;
+	tcPatch.pos201 = tcPatch.pos003 + edgeB030 * 2.0 / 3.0;
+	tcPatch.pos210 = tcPatch.pos300 + edgeB003 / 3.0;
+	tcPatch.pos120 = tcPatch.pos300 + edgeB003 * 2.0 / 3.0;
+
+	tcPatch.pos021 = 
+		projectToPlane(tcPatch.pos021, tcPatch.pos030, tcPatch.normal[0]);
+	tcPatch.pos012 =
+		projectToPlane(tcPatch.pos012, tcPatch.pos003, tcPatch.normal[1]);
+	tcPatch.pos102 = 
+		projectToPlane(tcPatch.pos102, tcPatch.pos003, tcPatch.normal[1]);
+	tcPatch.pos201 = 
+		projectToPlane(tcPatch.pos201, tcPatch.pos300, tcPatch.normal[2]);
+	tcPatch.pos210 = 
+		projectToPlane(tcPatch.pos210, tcPatch.pos300, tcPatch.normal[2]);
+	tcPatch.pos120 = 
+		projectToPlane(tcPatch.pos120, tcPatch.pos030, tcPatch.normal[0]);
+
+	// Handle the center
+	vec3 center = (tcPatch.pos003 + tcPatch.pos030 + tcPatch.pos300) / 3.0;
+	tcPatch.pos111 = (tcPatch.pos021 + tcPatch.pos012 + tcPatch.pos102 +
+		tcPatch.pos201 + tcPatch.pos210 + tcPatch.pos120) / 6.0;
+	tcPatch.pos111 += (tcPatch.pos111 - center) / 2.0;
+} 
 
 #define tessellatePositionNormalTangentTexCoord_DEFINED
 void tessellatePositionNormalTangentTexCoord(
 	in float tessLevelInner, 
 	in float tessLevelOuter)
 {
-	tcPosition[gl_InvocationID] = vPosition[gl_InvocationID];
-	tcTexCoords[gl_InvocationID] = vTexCoords[gl_InvocationID];
+	for(int i = 0 ; i < 3 ; i++) 
+	{		
+		tcPatch.texCoord[i] = vTexCoords[i];
+		tcPatch.normal[i] = vNormal[i];
 #if PASS_COLOR
-	tcNormal[gl_InvocationID] = vNormal[gl_InvocationID];
-	tcTangent[gl_InvocationID] = vTangent[gl_InvocationID];
+		tcPatch.tangent[i] = vTangent[i];
 #endif
-
-	if(gl_InvocationID == 0) 
-	{
-		gl_TessLevelInner[0] = tessLevelInner;
-		gl_TessLevelOuter[0] = tessLevelOuter;
-		gl_TessLevelOuter[1] = tessLevelOuter;
-		gl_TessLevelOuter[2] = tessLevelOuter;
 	}
+
+	calcPositions();
+
+	// Calculate the tessellation levels
+	gl_TessLevelOuter[0] = tessLevelOuter;
+	gl_TessLevelOuter[1] = tessLevelOuter;
+	gl_TessLevelOuter[2] = tessLevelOuter;
+	gl_TessLevelInner[0] = tessLevelInner;
 }
 
--- trunk/shaders/MsCommonTesse.glsl
+++ trunk/shaders/MsCommonTesse.glsl
@@ -1,15 +1,38 @@
 layout(triangles, equal_spacing, ccw) in;
 
 // Varyings in
+#if 0
 in highp vec3 tcPosition[];
 in highp vec2 tcTexCoords[];
 #if PASS_COLOR
 in mediump vec3 tcNormal[];
 in mediump vec4 tcTangent[];
 #endif
+#endif
+
+struct OutPatch
+{
+	vec3 pos030;
+	vec3 pos021;
+	vec3 pos012;
+	vec3 pos003;
+	vec3 pos102;
+	vec3 pos201;
+	vec3 pos300;
+	vec3 pos210;
+	vec3 pos120;
+	vec3 pos111;
+
+	vec2 texCoord[3];
+	vec3 normal[3];
+#if PASS_COLOR
+	vec4 tangent[3];
+#endif
+};
+
+in patch OutPatch tcPatch;
 
 // Varyings out
-out highp vec3 tePosition;
 out highp vec2 teTexCoords;
 #if PASS_COLOR
 out mediump vec3 teNormal;
@@ -23,12 +46,35 @@ out mediump vec4 teTangent;
 void subdivPositionNormalTangentTexCoord(in mat4 mvp, in mat3 normalMat)
 {
 #if PASS_COLOR
-	teNormal = normalize(normalMat * INTERPOLATE(tcNormal));
-	teTangent = INTERPOLATE(tcTangent);
+	teNormal = normalize(normalMat * INTERPOLATE(tcPatch.normal));
+	teTangent = INTERPOLATE(tcPatch.tangent);
 	teTangent.xyz = normalize(normalMat * teTangent.xyz);
 #endif
 
-	teTexCoords = INTERPOLATE(tcTexCoords);
+	teTexCoords = INTERPOLATE(tcPatch.texCoord);
 
-	gl_Position = mvp * vec4(INTERPOLATE(tcPosition), 1.0);
+	float u = gl_TessCoord.x;
+	float v = gl_TessCoord.y;
+	float w = gl_TessCoord.z;
+
+	float uPow3 = pow(u, 3);
+	float vPow3 = pow(v, 3);
+	float wPow3 = pow(w, 3);
+	float uPow2 = pow(u, 2);
+	float vPow2 = pow(v, 2);
+	float wPow2 = pow(w, 2);
+
+	vec3 pos = 
+		tcPatch.pos300 * wPow3
+		+ tcPatch.pos030 * uPow3
+		+ tcPatch.pos003 * vPow3
+		+ tcPatch.pos210 * 3.0 * wPow2 * u 
+		+ tcPatch.pos120 * 3.0 * w * uPow2 
+		+ tcPatch.pos201 * 3.0 * wPow2 * v 
+		+ tcPatch.pos021 * 3.0 * uPow2 * v 
+		+ tcPatch.pos102 * 3.0 * w * vPow2 
+		+ tcPatch.pos012 * 3.0 * u * vPow2 
+		+ tcPatch.pos111 * 6.0 * w * u * v;
+
+	gl_Position = mvp * vec4(pos, 1.0);
 }
