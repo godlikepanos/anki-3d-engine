b2f4d321b198adc8196b7a86fc9882208c1ccfa2
Panagiotis Christopoulos Charitos <godlikepanos@users.noreply.github.com>
2013-10-10 19:54:20 +0000

    Updating with a subdiv algorithm. It seems too expensive

--- shaders/MsCommonVert.glsl
+++ shaders/MsCommonVert.glsl
@@ -4,8 +4,12 @@
 /// @{
 layout(location = 0) in highp vec3 position;
 layout(location = 3) in highp vec2 texCoord;
-#if PASS_COLOR
+
+#if PASS_COLOR || TESSELLATION
 layout(location = 1) in mediump vec3 normal;
+#endif
+
+#if PASS_COLOR
 layout(location = 2) in mediump vec4 tangent;
 #endif
 /// @}
@@ -18,8 +22,11 @@ out highp vec2 vTexCoords;
 flat out uint vInstanceId;
 #endif
 
-#if PASS_COLOR
+#if PASS_COLOR || TESSELLATION
 out mediump vec3 vNormal;
+#endif
+
+#if PASS_COLOR
 out mediump vec4 vTangent;
 out mediump vec3 vVertPosViewSpace; ///< For env mapping. AKA view vector
 #endif
@@ -50,16 +57,23 @@ void writePositionTexCoord(in mat4 mvp)
 #define writePositionNormalTangentTexCoord_DEFINED
 void writePositionNormalTangentTexCoord(in mat4 mvp, in mat3 normalMat)
 {
-#if PASS_COLOR
-#	if TESSELLATION
+
+#if TESSELLATION
+
 	// Passthrough
 	vNormal = normal;
+#	if PASS_COLOR
 	vTangent = tangent;
-#	else
+#	endif
+
+#else
+
+#	if PASS_COLOR
 	vNormal = normalMat * normal;
 	vTangent.xyz = normalMat * tangent.xyz;
 	vTangent.w = tangent.w;
 #	endif
+
 #endif
 
 	writePositionTexCoord(mvp);
