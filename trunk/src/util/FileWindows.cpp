b67581eeee2f3385f0ac6f0641ecc8b57fd73cba
godlike@ancient-ritual.com <godlike@ancient-ritual.com@cf34f53d-7049-02d2-63c7-51c127bf19f8>
2014-06-23 17:06:02 +0000

    Some windows bits

--- trunk/src/util/FileWindows.cpp
+++ trunk/src/util/FileWindows.cpp
@@ -6,6 +6,7 @@
 #include "anki/util/File.h"
 #include "anki/util/Exception.h"
 #include "anki/util/Assert.h"
+#include <windows.h>
 
 namespace anki {
 
@@ -17,7 +18,11 @@ namespace anki {
 Bool File::fileExists(const char* filename)
 {
 	ANKI_ASSERT(filename);
-	// TODO
+
+	DWORD dwAttrib = GetFileAttributes(filename);
+
+	return dwAttrib != INVALID_FILE_ATTRIBUTES 
+		&& !(dwAttrib & FILE_ATTRIBUTE_DIRECTORY);
 }
 
 //==============================================================================
@@ -28,19 +33,40 @@ Bool File::fileExists(const char* filename)
 Bool directoryExists(const char* filename)
 {
 	ANKI_ASSERT(filename);
-	// TODO
+
+	DWORD dwAttrib = GetFileAttributes(filename);
+
+	return dwAttrib != INVALID_FILE_ATTRIBUTES 
+		&& (dwAttrib & FILE_ATTRIBUTE_DIRECTORY);
 }
 
 //==============================================================================
 void removeDirectory(const char* dirname)
 {
-	// TODO
+	ANKI_ASSERT(dirname);
+
+	SHFILEOPSTRUCTW fileOperation;
+	fileOperation.wFunc = FO_DELETE;
+	fileOperation.pFrom = dirname;
+	fileOperation.fFlags = FOF_NO_UI | FOF_NOCONFIRMATION;
+
+	I result = SHFileOperationW(&fileOperation);
+	if(result != 0) 
+	{
+		throw ANKI_EXCEPTION("Could not delete directory %s", dirname);
+	}
 }
 
 //==============================================================================
 void createDirectory(const char* dir)
 {
-	// TODO
+	ANKI_ASSERT(dir);
+
+	if(CreateDirectory(dir, NULL) 
+		|| GetLastError() == ERROR_ALREADY_EXISTS)
+	{
+		throw ANKI_EXCEPTION("Failed to create directory %s", dir);
+	}
 }
 
 } // end namespace anki
