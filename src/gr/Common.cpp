7e5bd78ddbd9e63848a0fe4edacd3a6261953a38
Panagiotis Christopoulos Charitos <godlike@ancient-ritual.com>
2016-08-15 19:17:27 +0200

    Add a forgotten file

---
+++ src/gr/Common.cpp
@@ -0,0 +1,15 @@
+// Copyright (C) 2009-2016, Panagiotis Christopoulos Charitos and contributors.
+// All rights reserved.
+// Code licensed under the BSD License.
+// http://www.anki3d.org/LICENSE
+
+#include <anki/gr/Common.h>
+
+namespace anki
+{
+
+//==============================================================================
+Array<CString, U(GpuVendor::COUNT)> GPU_VENDOR_STR = {
+	{"UNKNOWN", "ARM", "NVIDIA", "AMD", "INTEL"}};
+
+} // end namespace anki
