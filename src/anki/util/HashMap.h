7d7a993f8ed906f5e867b9283b70689acb7b0813
Panagiotis Christopoulos Charitos <godlike@ancient-ritual.com>
2016-12-31 13:43:10 +0100

    Update the copyright notice year

--- src/anki/util/HashMap.h
+++ src/anki/util/HashMap.h
@@ -1,4 +1,4 @@
-// Copyright (C) 2009-2016, Panagiotis Christopoulos Charitos and contributors.
+// Copyright (C) 2009-2017, Panagiotis Christopoulos Charitos and contributors.
 // All rights reserved.
 // Code licensed under the BSD License.
 // http://www.anki3d.org/LICENSE
--- tools/texture/convert_image.py
+++ tools/texture/convert_image.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-# Copyright (C) 2009-2016, Panagiotis Christopoulos Charitos and contributors.
+# Copyright (C) 2009-2017, Panagiotis Christopoulos Charitos and contributors.
 # All rights reserved.
 # Code licensed under the BSD License.
 # http://www.anki3d.org/LICENSE
