50619f346d729f4a365ccd6779cbf1f9a86e0857
Panagiotis Christopoulos Charitos <godlike@ancient-ritual.com>
2017-04-17 13:17:53 +0200

    Work on the exporter. It's almost feature complete

--- shaders/MsFsCommon.glsl
+++ shaders/MsFsCommon.glsl
@@ -8,33 +8,7 @@
 
 #include "shaders/Common.glsl"
 
-// Misc
-#define COLOR 0
-#define DEPTH 1
-
 #define CALC_BITANGENT_IN_VERT 1
-
-// Generic functions because materials cannot use operators
-#define add_DEFINED
-#define add(a, b) ((a) + (b))
-
-#define mul_DEFINED
-#define mul(a, b) ((a) * (b))
-
-#define assign_DEFINED
-#define assign(a, b) ((a) = (b))
-
-#define vec4ToVec3_DEFINED
-#define vec4ToVec3(a) ((a).xyz)
-
-#define vec3ToVec4_DEFINED
-#define vec3ToVec4(a, w) (vec4((a), (w)))
-
-#define getW_DEFINED
-#define getW(a) ((a).w)
-
-#define setW_DEFINED
-#define setW(a, b) ((a).w = (b))
 
 // Read from animated texture
 #define readAnimatedTextureRgba_DEFINED
