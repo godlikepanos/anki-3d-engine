fbbc7f856a5018d6475c9ff31cbbc47189bb5672
Panagiotis Christopoulos Charitos <godlike@ancient-ritual.com>
2017-05-14 21:29:57 +0200

    Add the skeleton for the TransferGpuMemoryAllocator

---
+++ src/anki/resource/TransferGpuMemoryAllocator.h
@@ -0,0 +1,23 @@
+// Copyright (C) 2009-2017, Panagiotis Christopoulos Charitos and contributors.
+// All rights reserved.
+// Code licensed under the BSD License.
+// http://www.anki3d.org/LICENSE
+
+#pragma once
+
+#include <anki/resource/Common.h>
+#include <anki/gr/common/ClassGpuAllocator.h>
+
+namespace anki
+{
+
+/// @addtogroup resource
+/// @{
+
+/// GPU memory allocator for GPU buffers used in transfer operations.
+class TransferGpuMemoryAllocator
+{
+};
+/// @}
+
+} // end namespace anki
