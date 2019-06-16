f8106a4b64d34a548fe8d2e260fcb02cb72a5246
Panagiotis Christopoulos Charitos <godlike@ancient-ritual.com>
2019-06-16 21:35:56 +0200

    Remove some unused stuff

--- tools/anki_scene_exporter/lib/math.py
+++
@@ -1,8 +0,0 @@
-# Copyright (C) 2009-2019, Panagiotis Christopoulos Charitos and contributors.
-# All rights reserved.
-# Code licensed under the BSD License.
-# http://www.anki3d.org/LICENSE
-# keep methods in alphabetical order
-
-def is_power_of_two(number):
-	return ((number & (number - 1)) == 0) and number != 0
