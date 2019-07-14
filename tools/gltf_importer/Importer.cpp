2eb8ce03199077fe1e39ed174b29a3b7b27d5a6c
Panagiotis Christopoulos Charitos <godlike@ancient-ritual.com>
2019-07-14 22:15:26 +0200

    Add the skeleton for the new GLTF importer

---
+++ tools/gltf_importer/Importer.cpp
@@ -0,0 +1,11 @@
+// Copyright (C) 2009-2019, Panagiotis Christopoulos Charitos and contributors.
+// All rights reserved.
+// Code licensed under the BSD License.
+// http://www.anki3d.org/LICENSE
+
+#include "Importer.h"
+
+namespace anki
+{
+
+} // end namespace anki
