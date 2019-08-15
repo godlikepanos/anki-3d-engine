a9df42ee1b18516bb8d95b51928a7671a8777de9
Panagiotis Christopoulos Charitos <godlike@ancient-ritual.com>
2019-08-15 22:08:46 +0200

    Try to fix the windows build. Try 2

--- src/anki/gr/vulkan/GrManagerImplSdl.cpp
+++ src/anki/gr/vulkan/GrManagerImplSdl.cpp
@@ -22,36 +22,11 @@ namespace anki
 
 Error GrManagerImpl::initSurface(const GrManagerInitInfo& init)
 {
-#if ANKI_OS == ANKI_OS_LINUX
 	if(!SDL_Vulkan_CreateSurface(init.m_window->getNative().m_window, m_instance, &m_surface))
 	{
 		ANKI_VK_LOGE("SDL_Vulkan_CreateSurface() failed: %s", SDL_GetError());
 		return Error::FUNCTION_FAILED;
 	}
-#elif ANKI_OS == ANKI_OS_WINDOWS
-	SDL_SysWMinfo wminfo;
-	SDL_VERSION(&wminfo.version);
-	if(!SDL_GetWindowWMInfo(init.m_window->getNative().m_window, &wminfo))
-	{
-		ANKI_VK_LOGE("SDL_GetWindowWMInfo() failed");
-		return Error::NONE;
-	}
-
-	Array<TCHAR, 512> className;
-	GetClassName(wminfo.info.win.window, &className[0], className.getSize());
-
-	WNDCLASS wce = {};
-	GetClassInfo(GetModuleHandle(NULL), &className[0], &wce);
-
-	VkWin32SurfaceCreateInfoKHR ci = {};
-	ci.sType = VK_STRUCTURE_TYPE_WIN32_SURFACE_CREATE_INFO_KHR;
-	ci.hinstance = wce.hInstance;
-	ci.hwnd = wminfo.info.win.window;
-
-	ANKI_VK_CHECK(vkCreateWin32SurfaceKHR(m_instance, &ci, nullptr, &m_surface));
-#else
-#	error TODO
-#endif
 
 	return Error::NONE;
 }
