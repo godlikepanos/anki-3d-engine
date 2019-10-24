2a4210eeb5202605cd5a0463f12115cb4d6c3497
Panagiotis Christopoulos Charitos <godlike@ancient-ritual.com>
2019-10-24 22:23:41 +0200

    Add the skeletons

---
+++ src/anki/shader_compiler/ShaderProgramCompiler.h
@@ -0,0 +1,6 @@
+// Copyright (C) 2009-2019, Panagiotis Christopoulos Charitos and contributors.
+// All rights reserved.
+// Code licensed under the BSD License.
+// http://www.anki3d.org/LICENSE
+
+#pragma once
