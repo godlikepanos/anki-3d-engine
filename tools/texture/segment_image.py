c9d5d78573f8960184c62436c06f399648a2df84
Panagiotis Christopoulos Charitos <godlike@ancient-ritual.com>
2020-01-16 21:19:15 +0100

    Update copyright notice

--- tools/texture/segment_image.py
+++ tools/texture/segment_image.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# Copyright (C) 2009-2019, Panagiotis Christopoulos Charitos and contributors.
+# Copyright (C) 2009-2020, Panagiotis Christopoulos Charitos and contributors.
 # All rights reserved.
 # Code licensed under the BSD License.
 # http://www.anki3d.org/LICENSE
