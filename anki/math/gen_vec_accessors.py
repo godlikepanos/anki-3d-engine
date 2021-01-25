9b074873ac705536ff87e4a21fae7e1f28ac1673
Panagiotis Christopoulos Charitos <godlike@ancient-ritual.com>
2021-01-25 20:32:50 +0100

    Update copyright header

--- anki/math/gen_vec_accessors.py
+++ anki/math/gen_vec_accessors.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-# Copyright (C) 2009-2020, Panagiotis Christopoulos Charitos and contributors.
+# Copyright (C) 2009-2021, Panagiotis Christopoulos Charitos and contributors.
 # All rights reserved.
 # Code licensed under the BSD License.
 # http://www.anki3d.org/LICENSE
