afa322e99840e4ebde426beb9392a8acbf864997
Panagiotis Christopoulos Charitos <godlike@ancient-ritual.com>
2022-05-13 08:35:17 +0200

    Not important

---
+++ AnKi/Shaders/Include/MiscRendererTypes.h
@@ -0,0 +1,12 @@
+// Copyright (C) 2009-2022, Panagiotis Christopoulos Charitos and contributors.
+// All rights reserved.
+// Code licensed under the BSD License.
+// http://www.anki3d.org/LICENSE
+
+#pragma once
+
+#include <AnKi/Shaders/Include/Common.h>
+
+ANKI_BEGIN_NAMESPACE
+
+ANKI_END_NAMESPACE
