b767165dc4e18b5d952a9246d9280479de96aefa
Panagiotis Christopoulos Charitos <godlike@ancient-ritual.com>
2023-04-11 22:19:49 +0200

    Add the render state bucket code

--- AnKi/Scene/RenderStateBucket.cpp
+++ AnKi/Scene/RenderStateBucket.cpp
@@ -4,3 +4,81 @@
 // http://www.anki3d.org/LICENSE
 
 #include <AnKi/Scene/RenderStateBucket.h>
+
+namespace anki {
+
+RenderStateBucketContainer::~RenderStateBucketContainer()
+{
+	for(RenderingTechnique t : EnumIterable<RenderingTechnique>())
+	{
+		for([[maybe_unused]] ExtendedBucket& b : m_buckets[t])
+		{
+			ANKI_ASSERT(!b.m_program.isCreated() && b.m_userCount == 0);
+		}
+	}
+}
+
+U32 RenderStateBucketContainer::addUser(const RenderStateInfo& state, RenderingTechnique technique)
+{
+	// Compute state gash
+	Array<U64, 3> toHash;
+	toHash[0] = state.m_program->getUuid();
+	toHash[1] = U64(state.m_primitiveTopology);
+	toHash[2] = state.m_indexedDrawcall;
+	const U64 hash = computeHash(toHash.getBegin(), toHash.getSizeInBytes());
+
+	SceneDynamicArray<ExtendedBucket>& buckets = m_buckets[technique];
+
+	LockGuard lock(m_mtx);
+
+	// Search bucket
+	for(U32 i = 0; i < buckets.getSize(); ++i)
+	{
+		if(buckets[i].m_hash == hash)
+		{
+			++buckets[i].m_userCount;
+
+			if(buckets[i].m_userCount == 1)
+			{
+				ANKI_ASSERT(!buckets[i].m_program.isCreated());
+				buckets[i].m_program = state.m_program;
+			}
+			else
+			{
+				ANKI_ASSERT(buckets[i].m_program.isCreated());
+			}
+
+			return i;
+		}
+	}
+
+	// Bucket not found, create one
+	ExtendedBucket& newBucket = *buckets.emplaceBack();
+	newBucket.m_hash = hash;
+	newBucket.m_indexedDrawcall = state.m_indexedDrawcall;
+	newBucket.m_primitiveTopology = state.m_primitiveTopology;
+	newBucket.m_program = state.m_program;
+	newBucket.m_userCount = 1;
+
+	return buckets.getSize() - 1;
+}
+
+void RenderStateBucketContainer::removeUser(U32 bucketIndex, RenderingTechnique technique)
+{
+	LockGuard lock(m_mtx);
+
+	ANKI_ASSERT(bucketIndex < m_buckets[technique].getSize());
+
+	ExtendedBucket& bucket = m_buckets[technique][bucketIndex];
+	ANKI_ASSERT(bucket.m_userCount > 0 && bucket.m_program.isCreated());
+
+	--bucket.m_userCount;
+
+	if(bucket.m_userCount == 0)
+	{
+		// No more users, make sure you release any references
+		bucket.m_program.reset(nullptr);
+	}
+}
+
+} // end namespace anki
--- AnKi/Scene/RenderStateBucket.h
+++ AnKi/Scene/RenderStateBucket.h
@@ -6,27 +6,45 @@
 #pragma once
 
 #include <AnKi/Scene/Common.h>
+#include <AnKi/Resource/RenderingKey.h>
 
 namespace anki {
 
 /// @addtogroup scene
 /// @{
 
+/// This is the unique render state of a renderable object in the scene.
 class RenderStateInfo
 {
 public:
 	ShaderProgramPtr m_program;
-	PrimitiveTopology m_primitiveTopology;
-	Bool m_indexed;
+	PrimitiveTopology m_primitiveTopology = PrimitiveTopology::kTriangles;
+	Bool m_indexedDrawcall = true;
 };
 
-/// TODO
+/// Holds an array of all render state buckets.
 class RenderStateBucketContainer
 {
 public:
-	U32 addUser(const RenderStateInfo& state);
+	~RenderStateBucketContainer();
 
-	void removeUser(U32 bucketIndex);
+	/// Add a new user for a specific render state and rendering technique.
+	U32 addUser(const RenderStateInfo& state, RenderingTechnique technique);
+
+	/// Remove the user.
+	void removeUser(U32 bucketIndex, RenderingTechnique technique);
+
+	template<typename TFunc>
+	void interateBuckets(RenderingTechnique technique, TFunc func) const
+	{
+		for(const ExtendedBucket& b : m_buckets[technique])
+		{
+			if(b.m_userCount > 0)
+			{
+				func(static_cast<const RenderStateInfo&>(b));
+			}
+		}
+	}
 
 private:
 	class ExtendedBucket : public RenderStateInfo
@@ -35,6 +53,9 @@ private:
 		U64 m_hash = 0;
 		U32 m_userCount = 0;
 	};
+
+	Array<SceneDynamicArray<ExtendedBucket>, U32(RenderingTechnique::kCount)> m_buckets;
+	Mutex m_mtx;
 };
 /// @}
 
